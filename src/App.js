f31f43d61d4e30035f180d266c2a028c5dec24e8
smeixner <sarameix22@gmail.com>
2022-08-29 11:32:43 -0400

    data display working

--- src/App.js
+++ src/App.js
@@ -11,25 +11,115 @@ import axios from 'axios';
 ////////////////
 
 import Header from './components/Header';
+import Show from './components/Show';
 
-////////////////////////
-// BODY HTML ELEMENTS //
-////////////////////////
+//////////////////
+// APP FUNCTION //
+//////////////////
 
 const App = () => {
+
+  ////////////
+  // STATES //
+  ////////////
+
+  let [shows, setShows] = useState([]);
+  let [newName, setNewName] = useState('');
+  let [newGenre, setNewGenre] = useState('');
+  let [newCreated, setNewCreated] = useState('');
+  let [newImage, setNewImage] = useState('');
+  let [newLastWatchedEp, setNewLastWatchedEp] = useState('');
+
+  //////////////////////
+  // HELPER FUNCTIONS //
+  //////////////////////
+
+  // *** READ *** //
+
+  // Function to Populate Show Data from BackEnd
+  const getShows = () => {
+    axios.get("https://fathomless-refuge-80112.herokuapp.com/shows/").then((response) => {
+      setShows(response.data);
+    })
+  }
+
+  // *** CREATE *** //
+
+  // Function to Update New Name from Add Form
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  }
+
+  // Function to Update New Genre from Add Form
+  const handleNewGenre = (event) => {
+    setNewGenre(event.target.value);
+  }
+
+  // Function to Update New Created from Add Form
+  const handleNewCreated = (event) => {
+    setNewCreated(event.target.value);
+  }
+
+  // Function to Update New Image from Add Form
+  const handleNewImage = (event) => {
+    setNewImage(event.target.value);
+  }
+
+  // Function to Update New Last Watched Episode from Add Form
+  const handleNewLastWatchedEp = (event) => {
+    setNewLastWatchedEp(event.target.value);
+  }
+
+  // Function to Handle Add Show Form Submission
+  const handleNewShowSubmit = (event) => {
+    // Prevent Form Default
+    event.preventDefault();
+
+    console.log(event);
+  }
+
+  ////////////////
+  // USE EFFECT //
+  ////////////////
+
+  // Use Effect to Populate Show Data
+  useEffect(()=>{
+    getShows();
+  }, [])
+
+  ////////////////////////
+  // BODY HTML ELEMENTS //
+  ////////////////////////
+
   return (
     <>
       <Header/>
       <main>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
-        <h1>Hello World!</h1>
+        <section>
+          <h1>Add New Show</h1>
+          <form className='add-form' onSubmit={handleNewShowSubmit}>
+            <div className='form-group'>
+              <label htmlFor="showNameInput">Name</label>
+              <input type="text" className="form-control" id="showNameInput" name="image" onChange={handleNewName}/>
+              <input className="submit-button" type="submit" value="Add New Show"/>
+            </div>
+          </form>
+        </section>
+        <section>
+          <h1>My Watch List</h1>
+          <div className='shows-container'>
+            {
+              shows.map((show) => {
+                return (
+                  <div className='show-container' key={show._id}>
+                    <Show show={show}/>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </section>
+        
       </main>
     </>
   );
---
+++ src/components/Show.js
@@ -0,0 +1,28 @@
+const Show = (props) => {
+    // Make Last Watched Date from Data
+    let watchDate = props.show.updatedAt.split('-');
+    watchDate = [watchDate[1], watchDate[2].substring(0,2), watchDate[0]];
+    watchDate = watchDate.join('-');
+
+    let watchTime = props.show.updatedAt;
+    console.log(watchTime);
+
+    // Return HTML Elements
+    return (
+        <>
+            <img src={props.show.image}/>
+            <div className="show-info-container">
+                <div className="show-info-container-left">
+                    <h2>{props.show.name}</h2>
+                    <p>{props.show.lastWatchedEp}</p>
+                </div>
+                <div className="show-info-container-right">
+                    <h4>Last Watched:</h4>
+                    <p>{watchDate}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Show;
